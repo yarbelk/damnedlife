051ee73f907c9fb4ba4c2d45dda25c3b0f6b6c72
= <=>
2016-05-18 11:01:03 +0800

    Document the shapes

--- game/shapes.go
+++ game/shapes.go
@@ -1,5 +1,12 @@
+/* Shapes for the game.  pass in an offset and the
+board you want to add the shape to.
+*/
 package game
 
+// Glider
+// X..
+// .XX
+// XX.
 func Glider(b *Board, x, y int) {
 	b.SetAlive(x+0, y+0)
 	b.SetAlive(x+1, y+1)
@@ -8,6 +15,11 @@ func Glider(b *Board, x, y int) {
 	b.SetAlive(x+1, y+2)
 }
 
+// LWSS Lightweight Space Ship
+// .XX..
+// XXXX.
+// XX.XX
+// ..XX.
 func LWSS(b *Board, x, y int) {
 	b.SetAlive(x+1, y+0)
 	b.SetAlive(x+2, y+0)
